4e8c7e89c3b7a9f5110b2f4f69bdb040108c6c91
Yasuda Kouki <e225703@ie.u-ryukyu.ac.jp>
2022-12-11 23:40:54 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch(NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }
