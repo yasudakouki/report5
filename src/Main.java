d23ba8bf6a3f8b84f61051d06a246e01d3bf854e
Yasuda Kouki <e225703@ie.u-ryukyu.ac.jp>
2022-12-11 23:59:06 +0900

    step3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch(NullPointerException e) {
-            System.out.println("NullPointerExceptionが発生しました");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
